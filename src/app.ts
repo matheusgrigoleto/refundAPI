ea14857df28e7c607abee613c20c4bfbe0308f03
Matheus Grigoleto <marthexmit@gmail.com>
2025-08-29 16:55:05 -0300

    FEAT: Created error handling

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
+import { errorHandling } from "./middlewares/error-handling";
 
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("hello world");
 });
+
+app.use(errorHandling);
 
 export { app };
